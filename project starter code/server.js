f5bc46cc166f0b8bda5a2d720314d37ca04adbe8
ajeyahegde <ashegde@usc.edu>
2024-03-02 13:48:28 -0800

    Patch-1

--- project starter code/server.js
+++ project starter code/server.js
@@ -56,6 +56,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
       });
 
     } catch(error) {
+      console.error(error)
         return res.status(500).send("Could not filter the image");
     }
   } );
